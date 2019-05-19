10890bb0fde5fe365ec85a5887cc7ceea8e78ee1
Ahmadreza <ahmadrezaanazari@gmail.com>
2019-05-19 17:43:41 +0430

    Q2 example

---
+++ Python/Q2/main.py
@@ -0,0 +1,11 @@
+def render(request, htmlFileName, *args, **keywords):
+    print('Html file name:', htmlFileName)
+    print('I recieved these datas:')
+    [print(arg, end=' ') for arg in args]
+    for key in keywords:
+        if key is 'title':
+            print('your title is:', keywords[key])
+        elif key is 'error':
+            print('your error code is:', keywords[key])
+
+render(None, 'test.html', 1, 2, 3, 4, title='testing', error='404')
