1ae542fe3a96ef506e9bb1d6c6e25f07bc515742
Ahmadreza <ahmadrezaanazari@gmail.com>
2019-05-26 12:56:38 +0430

    Q5 almost done

--- Python/Q5/main.py
+++ Python/Q5/main.py
@@ -3,8 +3,8 @@ import os
 
 def getInput():
     while(True):
-        inputStr = input("Enter your command, type quite or exit to exit: \n")
-        if(inputStr == 'quite' or inputStr == 'exit'):
+        inputStr = input("Enter your command, type quit or exit to exit: \n")
+        if(inputStr == 'quit' or inputStr == 'exit'):
             raise Exception("Good Bye")
         inputStr = inputStr.split('(')
         if(len(inputStr) != 2):
@@ -15,47 +15,57 @@ def getInput():
             if(len(temp) != 2):
                 print("wrong format")
             else:
-                path = temp[1][:-1] + '/' + temp[0]
-                if(len(temp[1][:-1]) == 0):
+                path = temp[1][:-1]
+                if path.startswith(' '):
                     path = path[1:]
-                return [command, path]
+                name = temp[0]
+                command = command.lower()
+                return [command, path, name]
 
 
-while(True):
-    try:
-        [command, path] = getInput()
-        print(command, path)
-    except:
-        break
+def find(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+    return result
 
-    if command == 'create_dir':
+if __name__ == "__main__":
+    while(True):
         try:
-            os.mkdir(path)
-        except OSError:
-            print(f"Creation of the directory {path} failed")
+            [command, path, name] = getInput()
+            if(len(path) == 0):
+                path = os.getcwd()
+        except:
+            break
+        if command == 'create_dir':
+            try:
+                os.mkdir(path + '/' + name)
+            except OSError:
+                print(f"Creation of the directory {name} in {path} failed")
+            else:
+                print(f"Successfully created the directory {path}")
+        elif command == 'create_file':
+            try:
+                os.mknod(path + '/' + name)
+            except OSError:
+                print(f"Creation of the file {name} in {path} failed")
+            else:
+                print(f"Successfully created the file {name} in {path}")
+        elif command == 'delete':
+            try:
+                os.remove(path + '/' + name)
+            except OSError:
+                print(f"Deletion of the file {name} in {path} failed")
+            else:
+                print(f"Successfully deleted the file {name} in {path}")
+        elif command == 'find':
+            try:
+                result = find(name, path)
+            except OSError:
+                print(f"Finding of the file {name} failed")
+            else:
+                print(f"Successfully found the file(s) {name}")
+                print(result)
         else:
-            print(f"Successfully created the directory {path}")
-    elif command == 'create_file':
-        try:
-            os.mknod(path)
-        except OSError:
-            print(f"Creation of the file {path} failed")
-        else:
-            print(f"Successfully created the file {path}")
-    elif command == 'delete':
-        try:
-            print(path)
-            os.remove(path)
-        except OSError:
-            print(f"Deletion of the file {path} failed")
-        else:
-            print(f"Successfully deleted the file {path}")
-    elif command == 'find':
-        try:
-            pass
-        except OSError:
-            print(f"Creation of the directory {path} failed")
-        else:
-            print(f"Successfully created the directory {path}")
-    else:
-        print("wrong format")
+            print("wrong format")
